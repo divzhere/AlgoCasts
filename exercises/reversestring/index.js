15d55f83ece63d1c80178d47b9df7a2b11147874
Divyum <divz7777@gmail.com>
2020-04-11 07:37:14 +0530

    rev string solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -8,12 +8,26 @@
 
 function reverse(str) {
   //my solution
-    let arr = str.split("");
-    function reducer(currentValue){
-        return currentValue
-    }
-    arr.reverse().reduce(reducer);
-    return arr.toString().replace(/,/g, "")
+    // let arr = str.split("");
+    // function reducer(currentValue){
+    //     return currentValue
+    // }
+    // arr.reverse().reduce(reducer);
+    // return arr.toString().replace(/,/g, "")
+
+    //solution #1
+    //return str.split('').reverse().join('');
+    
+    //solution #2
+    // let reversed = '';
+    // for (let character of str) {
+    //   reversed = character + reversed;
+    // }
+    // return reversed
+
+    //solution #3 [kinda similar to my solution]
+     return str.split('').reduce((rev, char)=> char + rev,'');
+    //Reducer takes two values/arguments, an arrow function and an initial value
 }
 
 module.exports = reverse;
